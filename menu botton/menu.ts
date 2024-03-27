503ba879517fddd223020fc3bdb5e394e6ad90e2
ytpen <160297029+penyt@users.noreply.github.com>
2024-03-27 15:08:24 +0800

    Create menu.ts

---
+++ menu botton/menu.ts
@@ -0,0 +1,11 @@
+export default function () {
+const leftMenu = document.getElementById('left-menu');
+    if (leftMenu) {
+        leftMenu.addEventListener('click', () => {
+            if (document.getElementById('lmenu').classList.contains('transiting')) return;
+            document.body.classList.toggle('show-left-menu');
+            slideToggle(document.getElementById('lmenu'), 300);
+            leftMenu.classList.toggle('is-left-active');
+        });
+    }
+}
